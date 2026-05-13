3a001099fc8f709e45117fc9787c43d8962a204b
Apple <saravyas007@gmail.com>
2026-05-14 00:15:22 +0530

    ✨ Resume review
    added resume review comp

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import ScoreBadge from "./ScoreBadge";
+import ScoreGauge from "./ScoreGauge";
 
-const Summary = ({ feedback }) => {
-  return <div>Summary</div>;
+const Category = ({ title, score }: { title: string; score: number }) => {
+  const textColor =
+    score > 70
+      ? "text-green-600"
+      : score > 49
+        ? "text-yellow-600"
+        : "text-red-600";
+  return (
+    <div className="resume-summary">
+      <div className="category">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <p className="text-2xl">{title}</p>
+          <ScoreBadge score={score} />
+        </div>
+        <p className="text-2xl">
+          <span className={textColor}>{score}</span>/100
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Summary = ({ feedback }: { feedback: Feedback }) => {
+  return (
+    <div className="bg-white rounded-2xl shadow-md w-full">
+      <div className="flex flex-row items-center gap-8 p-4">
+        <ScoreGauge score={feedback.overallScore} />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-2xl font-bold">Your Resume Score</h2>
+          <p className="text-sm text-gray-500">
+            Thhis score calculated based on variable below
+          </p>
+        </div>
+      </div>
+      <Category score={feedback.toneAndStyle.score} title="Tone and Style" />
+      <Category score={feedback.content.score} title="Content" />
+      <Category score={feedback.structure.score} title="Structure" />
+      <Category score={feedback.skills.score} title="Skills" />
+    </div>
+  );
 };
 
 export default Summary;
