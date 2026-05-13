fcf95e13272c565d3393944118eecb6eca47d155
Apple <saravyas007@gmail.com>
2026-05-13 23:05:04 +0530

    ✨ Resume Feedback
    added resume feedback view pending with data

---
+++ app/components/ATS.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ATS = ({ score, suggestions }) => {
+  return <div>ATS</div>;
+};
+
+export default ATS;
---
+++ app/components/Details.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = ({ feedback }) => {
+  return <div>Details</div>;
+};
+
+export default Details;
